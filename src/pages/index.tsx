625bc21a342aa1330d47c8918aae834cc03fd620
scattman105 <c.zhang@me.com>
2025-07-16 21:44:09 -0500

    Test minimal render

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-
-const Home = () => {
-  return <h1>If you can see this, routing is working ✅</h1>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <>
+      <p>Hello from a statically rendered page.</p>
+    </>
+  );
+}
